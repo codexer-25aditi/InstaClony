47dd6111a3c6332d4b1efa794cc4f00f282e060d
Aditi <gudisahu25@gmail.com>
2023-07-11 21:35:07 +0530

    Edit

---
+++ insta-clone/src/App.js
@@ -0,0 +1,11 @@
+import Auth from "./modules/Authorization";
+import Form from "./modules/Authorization"
+
+
+function App() {
+  return (
+   <Auth/>
+  );
+}
+
+export default App;
---
+++ insta-clone/src/modules/Authorization/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Form from "./Form";
+const Auth = ()=>{
+    return(
+       <Form/> 
+    )
+
+    
+
+}
+
+export default Auth;
